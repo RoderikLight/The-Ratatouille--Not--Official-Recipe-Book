189c5421d85cab5b484ae4341a8c2a7b8b2980e9
RoderikLight <itzrodrixxx@gmail.com>
2023-11-28 21:52:46 -0700

    Barebone build assembled
    basic buttons n stuff

---
+++ app/src/main/java/com/roderiklight/ratatouillerecipebook/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.roderiklight.ratatouillerecipebook
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val i = Intent(this, MainActivity::class.java)
+
+        val timer = object: CountDownTimer(3000, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+
+            override fun onFinish() {
+                startActivity(i)
+                finish()
+            }
+        }
+        timer.start()
+    }
+}
